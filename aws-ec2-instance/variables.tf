f9e081dc433f41f34c9dcc00958c731c751227e6
Yash Khemani <yash@hashicorp.com>
2019-02-08 13:15:05 +0000

    updating default ami

--- aws-ec2-instance/variables.tf
+++ aws-ec2-instance/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default = "ami-03a935aafa6b52b97"
 }
 
 variable "instance_type" {
